b78a67f2b033e164a1cff6b5ed4d2b070d1708f5
mtkilic <kilicmahmut@ymail.com>
2017-02-10 10:24:11 -0500

    First commit

---
+++ html_entity.py
@@ -0,0 +1,31 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  10 9:37:58 2017
+
+@author: kilicm
+"""
+
+
+import os
+import codecs
+import re
+
+
+def entity2char(word):
+    for filenames in  os.listdir('.'):
+        if filenames.endswith('.txt'):
+            with codecs.open(filenames, 'w', encoding='utf-8-sig') as f:
+                for line in lines:
+                    
+                    for word in line.split():
+                        result = re.sub(r"&#(\d+);", lambda m: chr(int(m.group(1))), word)
+                        #print(result)
+                        f.write(result)
+                        f.write(' ')
+
+for filenames in  os.listdir('.'):
+    if filenames.endswith('.txt'):
+        with codecs.open(filenames, 'r', encoding='utf-8-sig') as f:
+            lines = f.readlines()
+            
+            entity2char(lines)
